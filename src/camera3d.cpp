b317bc967fd309b22e1c0140f93af21b35f890e3
LiloMarino <lilo.marino@hotmail.com>
2025-02-17 17:03:47 -0300

    refactor: :recycle: Documentado e melhorado classe da Camera3D

--- src/camera3d.cpp
+++ src/camera3d.cpp
@@ -1,12 +1,15 @@
 #include "camera3d.hpp"
 
-// A implementação dos métodos já está incluída no cabeçalho.
-
-Camera3D::Camera3D(double eyeX, double eyeY, double eyeZ, double rotX, double rotY, double rotZ, double focalLength, double sensorSize, double aspectRatio, double nearPlane, double farPlane)
-    : eyeX(eyeX), eyeY(eyeY), eyeZ(eyeZ), rotX(rotX), rotY(rotY), rotZ(rotZ), focalLength(focalLength), sensorSize(sensorSize), aspectRatio(aspectRatio), nearPlane(nearPlane), farPlane(farPlane)
+Camera3D::Camera3D(double eyeX, double eyeY, double eyeZ, double rotX, double rotY, double rotZ,
+                   double focalLength, double sensorSize, double aspectRatio, double nearPlane, double farPlane)
+    : eyeX(eyeX), eyeY(eyeY), eyeZ(eyeZ),
+      aspectRatio(aspectRatio), nearPlane(nearPlane), farPlane(farPlane)
 {
-    calculateFOV();
-    calculateDirection();
+    // Calcula o campo de visão (FOV) baseado na lente e no sensor do Blender
+    fov = calculateFOV(focalLength, sensorSize);
+
+    // Calcula a direção da câmera baseada na rotação
+    calculateDirection(rotX, rotY, rotZ);
 }
 
 void Camera3D::applyCamera() const
@@ -17,24 +20,22 @@ void Camera3D::applyCamera() const
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    gluLookAt(eyeX, eyeY, eyeZ, centerX, centerY, centerZ, upX, upY, upZ);
+    gluLookAt(eyeX, eyeY, eyeZ,
+              eyeX + dirX, eyeY + dirY, eyeZ + dirZ, // Centro da câmera (para onde está olhando)
+              0.0, 0.0, 1.0);                        // Vetor "para cima" fixo no eixo Z (Por causa do Blender)
 }
 
-void Camera3D::calculateFOV()
+double Camera3D::calculateFOV(double focalLength, double sensorSize) const
 {
-    fov = 2.0 * atan((sensorSize / 2.0) / focalLength) * (180.0 / M_PI);
+    return 2.0 * atan((sensorSize / 2.0) / focalLength) * (180.0 / M_PI);
 }
 
-void Camera3D::calculateDirection()
+void Camera3D::calculateDirection(double rotX, double rotY, double rotZ)
 {
     double yaw = rotY * (M_PI / 180.0);
     double pitch = rotX * (M_PI / 180.0);
 
-    centerX = eyeX + cos(yaw) * cos(pitch);
-    centerY = eyeY + sin(pitch);
-    centerZ = eyeZ + sin(yaw) * cos(pitch);
-
-    upX = 0.0;
-    upY = 0.0;
-    upZ = 1.0;
+    dirX = cos(yaw) * cos(pitch);
+    dirY = sin(pitch);
+    dirZ = sin(yaw) * cos(pitch);
 }
--- src/camera3d.hpp
+++ src/camera3d.hpp
@@ -7,26 +7,47 @@
 class Camera3D
 {
 private:
-    double eyeX, eyeY, eyeZ;
-    double rotX, rotY, rotZ;
-    double focalLength;
-    double sensorSize;
-    double aspectRatio;
-    double nearPlane;
-    double farPlane;
-    double fov;
-    double centerX, centerY, centerZ;
-    double upX, upY, upZ;
+    double eyeX, eyeY, eyeZ;   ///< Posição da câmera no espaço 3D.
+    double dirX, dirY, dirZ;   ///< Direção da câmera.
+    double fov;                ///< Campo de visão vertical.
+    double aspectRatio;        ///< Proporção da tela.
+    double nearPlane;          ///< Plano próximo de corte da câmera.
+    double farPlane;           ///< Plano distante de corte da câmera.
 
 public:
-    Camera3D(double eyeX, double eyeY, double eyeZ, double rotX, double rotY, double rotZ, double focalLength, double sensorSize, double aspectRatio, double nearPlane, double farPlane);
+    /**
+     * @brief Construtor da câmera 3D baseado em parâmetros do Blender.
+     * 
+     * @param eyeX Posição X da câmera.
+     * @param eyeY Posição Y da câmera.
+     * @param eyeZ Posição Z da câmera.
+     * @param rotX Rotação em torno do eixo X (ângulo do Blender).
+     * @param rotY Rotação em torno do eixo Y (ângulo do Blender).
+     * @param rotZ Rotação em torno do eixo Z (ângulo do Blender).
+     * @param focalLength Distância focal da câmera.
+     * @param sensorSize Tamanho do sensor da câmera.
+     * @param aspectRatio Proporção da tela.
+     * @param nearPlane Distância do plano próximo de renderização.
+     * @param farPlane Distância do plano distante de renderização.
+     */
+    Camera3D(double eyeX, double eyeY, double eyeZ, double rotX, double rotY, double rotZ, 
+             double focalLength, double sensorSize, double aspectRatio, double nearPlane, double farPlane);
 
+    /**
+     * @brief Aplica as configurações da câmera no OpenGL.
+     */
     void applyCamera() const;
 
 private:
-    void calculateFOV();
-    
-    void calculateDirection();
+    /**
+     * @brief Calcula o campo de visão (FOV) com base na distância focal e no tamanho do sensor.
+     */
+    double calculateFOV(double focalLength, double sensorSize) const;
+
+    /**
+     * @brief Calcula a direção para onde a câmera está olhando com base nas rotações.
+     */
+    void calculateDirection(double rotX, double rotY, double rotZ);
 };
 
 #endif
